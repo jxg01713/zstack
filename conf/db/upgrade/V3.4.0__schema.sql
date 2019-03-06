8cff4026f884045860b8dbe1a82f82215ba389f7
Wei Wang <wei.wang@zstack.io>
2019-03-06 22:41:51 +0800

    Implement mini storage

--- conf/db/upgrade/V3.4.0__schema.sql
+++ conf/db/upgrade/V3.4.0__schema.sql
@@ -208,3 +208,41 @@ CREATE TABLE `ResourceUsageVO` (
 ) ENGINE=INNODB DEFAULT CHARSET=UTF8;
 
 INSERT INTO AccountResourceRefVO (`accountUuid`, `ownerAccountUuid`, `resourceUuid`, `resourceType`, `permission`, `isShared`, `lastOpDate`, `createDate`) SELECT "36c27e8ff05c4780bf6d2fa65700f22e", "36c27e8ff05c4780bf6d2fa65700f22e", t.uuid, "L2NetworkVO", 2, 0, CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP() FROM L2NetworkVO t where t.type in ("L2VlanNetwork", "L2NoVlanNetwork");
+
+-- ----------------------------
+--  For mini storage
+-- ----------------------------
+
+CREATE TABLE `zstack`.`MiniStorageResourceReplicationVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE COMMENT 'mini storage replications',
+    `type` varchar(32) NOT NULL COMMENT 'resource type',
+    `state` varchar(32) NOT NULL COMMENT 'replication state',
+    `resourceUuid` varchar(32) NOT NULL COMMENT 'resource uuid',
+    `size` BIGINT UNSIGNED DEFAULT 0 COMMENT 'resource size',
+    `port` BIGINT UNSIGNED DEFAULT 0 COMMENT 'resource port on host',
+    `hostUuid` varchar(32) NOT NULL COMMENT 'host',
+    `primaryStorageUuid` varchar(32) NOT NULL COMMENT 'primary storage uuid',
+    `networkStatus` varchar(32) DEFAULT NULL COMMENT 'replication network status',
+    `diskStatus` varchar(32) DEFAULT NULL COMMENT 'replication disk status',
+    `role` varchar(32) DEFAULT NULL COMMENT 'replication role',
+    `lastOpDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE CURRENT_TIMESTAMP,
+    `createDate` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+    PRIMARY KEY (`uuid`),
+    CONSTRAINT `fkMiniStorageResourceReplicationVOPrimaryStorageEO` FOREIGN KEY (`primaryStorageUuid`) REFERENCES `PrimaryStorageEO` (uuid) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `zstack`.`MiniStorageVO` (
+    `uuid` varchar(32) NOT NULL UNIQUE COMMENT 'mini storage',
+    `miniStorageType` varchar(32) NOT NULL COMMENT 'type',
+    `diskIdentifier` varchar(255) DEFAULT NULL COMMENT 'disk wwid/wwn/etc',
+    PRIMARY KEY (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE  `zstack`.`MiniStorageHostRefVO` (
+    `id` BIGINT UNSIGNED NOT NULL,
+    `totalCapacity` BIGINT UNSIGNED DEFAULT 0,
+    `availableCapacity` BIGINT UNSIGNED DEFAULT 0,
+    `totalPhysicalCapacity` BIGINT UNSIGNED DEFAULT 0,
+    `availablePhysicalCapacity` BIGINT UNSIGNED DEFAULT 0,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
