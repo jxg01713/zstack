65db3ec1907334c6ed8af9176c51fae7bd33d02b
Alan Jager <alanjager@outlook.com>
2019-05-27 11:04:51 +0800

    Update sdk
    Signed-off-by: AlanJager <alanjager@outlook.com>

--- sdk/src/main/java/org/zstack/sdk/iam2/api/GetIAM2RoleTemplateResult.java
+++ sdk/src/main/java/org/zstack/sdk/iam2/api/GetIAM2RoleTemplateResult.java
@@ -3,12 +3,12 @@ package org.zstack.sdk.iam2.api;
 
 
 public class GetIAM2RoleTemplateResult {
-    public java.util.List inventories;
-    public void setInventories(java.util.List inventories) {
-        this.inventories = inventories;
+    public java.util.List templates;
+    public void setTemplates(java.util.List templates) {
+        this.templates = templates;
     }
-    public java.util.List getInventories() {
-        return this.inventories;
+    public java.util.List getTemplates() {
+        return this.templates;
     }
 
 }
